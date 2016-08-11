8c568cd4f83d537f81b2d5fe6f11dc1def4eaea5
Francis Prus and Jeremy Fryd <jeremy@>
2016-08-11 10:12:05 -0700

    Add js code and survey options (needs debugging).

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
-  var age = parseInt(prompt("How old are you?"));
+  // var age = parseInt(prompt("How old are you?"));
 
-  if (age > 18) {
-    $('#over18').show();
-  } else if (age === 18) {
-    alert("Vote for Trout");
-    $('#over18').show();
-  } else {
-    $('#under18').show();
-  }
+  // if (age > 18) {
+  //   $('#over18').show();
+  // } else if (age === 18) {
+  //   alert("Vote for Trout");
+  //   $('#over18').show();
+  // } else {
+  //   $('#under18').show();
+  // }
+
+
+  $("form#voting").submit(function(event) {
+    event.preventDefault();
+    var idea = $("form#ideologyid").val();
+    if (idea === "Liberal") {
+      alert("congratulations, you're a democrat!");
+    }
+    else if (choice === "anti-abortion") {
+      alert("congrats, you're a republican");
+    }
+
+  });
+
 });
